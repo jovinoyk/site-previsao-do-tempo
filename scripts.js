653969a710cfdd4f11334b4c25932e0dd11b74ea
Maykon Jovino <maykon.jovino@gmail.com>
2024-04-10 19:32:06 -0300

    Comit com alterações

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const key = "9d9ee89b3696e4983b83bd8f7680a9d7"
+
+
+function colocarDadosNaTela(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".tempo").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src =  `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+
+
+async function buscarCidade(cidade){
+
+    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+    
+    colocarDadosNaTela(dados)
+}
+
+function clickButton() {
+    const cidade = document.querySelector(".input-cidade").value
+    
+    buscarCidade(cidade)
+  
+}
+
